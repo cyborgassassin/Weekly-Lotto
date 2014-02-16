02fbdce7988991b86fe4c4dc310818862f0b317b
KyleMassacre <ky.ellis83@gmail.com>
2014-02-16 10:32:57 -0800

    Update install.sql
    created new table for weekly winners display

--- install.sql
+++ install.sql
@@ -1,4 +1,11 @@
 CREATE TABLE IF NOT EXISTS `weekly_lottery` (
-          `Uid` int(10) NOT NULL,
-          `amount` MEDIUMINT UNSIGNED NOT NULL
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+    `Uid` int(10) NOT NULL,
+    `amount` MEDIUMINT UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
+CREATE TABLE `weekly_lottery_winners` (
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+	`userid` int(10) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
